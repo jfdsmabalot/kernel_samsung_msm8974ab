e4dddad542f7882d4be0b6a98b413bfbdf1e960b
kiko <jfdsmabalot@gmail.com>
2014-05-12 06:30:55 +0000

    InteractiveX Add InteractiveX as one of the default governors.
    Initial Adaptation for the S5
    
    Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -440,6 +440,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 
